37ab56f8857f13017f13c1f46182913e7acb687c
masud001 <madhnagar@gmail.com>
2021-09-26 23:13:43 +0600

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+// this code is responcibol for remaining time counter
+(function () {
+	const second = 1000,
+		minute = second * 60,
+		hour = minute * 60,
+		day = hour * 24;
+
+	let testTime = 'Oct 30, 2021 00:00:00',
+		countDown = new Date(testTime).getTime(),
+		x = setInterval(function () {
+			let now = new Date().getTime(),
+				distance = countDown - now;
+
+			(document.getElementById('days').innerText = Math.floor(distance / day)),
+				(document.getElementById('hours').innerText = Math.floor((distance % day) / hour)),
+				(document.getElementById('minutes').innerText = Math.floor(
+					(distance % hour) / minute
+				)),
+				(document.getElementById('seconds').innerText = Math.floor(
+					(distance % minute) / second
+				));
+
+			//do something later when date is reached
+			if (distance < 0) {
+				let countdown = document.getElementById('countdown');
+
+				countdown.style.display = 'none';
+
+				clearInterval(x);
+			}
+			//seconds
+		}, 0);
+})();
+
+// init Magnific Popup js library
+
+$(document).ready(function () {
+	$('.jsVideoPlay').magnificPopup({
+		type: 'iframe',
+	});
+});
